e62a432a5e3215b6165abf0d2794a36bfbece899
Guilherme <Guilhermesilvas2004@gmail.com>
2023-09-29 20:52:41 -0300

    CRUD com react

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,17 @@
+import {} from 'react'
+import { GiAlliedStar as Estrela} from "react-icons/gi";
+import { FaGithub as Git } from "react-icons/fa6";
+
+
+
+
+function Ropade (){
+  return(
+    <section >
+    <Estrela/>
+    <Git/>
+    <h3>@2023-todos os direitos reservados</h3>
+    </section>
+  )
+}
+export default Ropade
